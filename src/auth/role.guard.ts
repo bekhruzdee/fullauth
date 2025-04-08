4dbbb1cf85d4f2ea341759779142ab162dd58c5e
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-04-08 13:07:42 +0500

    add auth and users

---
+++ src/auth/role.guard.ts
@@ -0,0 +1,27 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException,
+  } from '@nestjs/common';
+  
+  @Injectable()
+  export class RolesGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean {
+      const request = context.switchToHttp().getRequest();
+      const user = request.user;
+  
+      if (!user || !user.role) {
+        throw new UnauthorizedException(
+          'User is not authenticated or role is missing',
+        );
+      }
+  
+      if (user.role !== 'admin') {
+        throw new UnauthorizedException('You are not allowed');
+      }
+  
+      return true;
+    }
+  }
+  
